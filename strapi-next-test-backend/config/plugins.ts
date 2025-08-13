aa15cb6477c490e6b9c716b34b4481e1729bb95b
Claudia <claudae_f@hotmail.com>
2025-08-13 22:00:48 +0200

    update config for navigation-plugin

--- strapi-next-test-backend/config/plugins.ts
+++ strapi-next-test-backend/config/plugins.ts
@@ -2,5 +2,22 @@ export default () =>
   ({ env }) => ({
     "entity-notes": {
       enabled: true,
-    }
+    },
+    navigation: {
+      enabled: true,
+      config: {
+          contentTypes: ['api::page.page'],
+          defaultContentTypes: ['api::page.page'],
+          contentTypesNameFields: {
+              'api::page.page': ['title']
+          },
+          pathDefaultFields: {
+              'api::page.page': ['url']
+          },
+          allowedLevels: 3,
+          gql: {
+            navigationItemRelated: ['Page']
+          },
+      }
+  }
   });
